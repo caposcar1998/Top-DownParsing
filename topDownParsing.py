e252227663f1c60644b2e9c8b6a4416911a4a40c
Mau Acosta <chafito@live.com>
2020-06-03 14:11:24 -0500

    Algoritmo Funciona :))))))

--- topDownParsing.py
+++ topDownParsing.py
@@ -9,25 +9,31 @@ def createParsingTree(enterString, maxSizeTree, nonTerminalSymbols,startingSymbo
     print(res[0])
     for prod in listProductions.get(res[0]):
         counter = 0
+        print(prod)
         getNodesRec(enterString, prod, maxSizeTree, nonTerminalSymbols, listProductions, counter)
 
 def getNodesRec(enterString, prodString, maxSizeTree, nonTerminalSymbols, listProductions, counter):
     counter += 1
-    print(counter)
     if(enterString == prodString or counter >= int(maxSizeTree)):
-        if(enterString == prodString):
-            print('Valor Final ' + prodString)
-        else:
-            print('Altura Maxima encontrada. ' + prodString)
+        #if(enterString == prodString):
+            #print('Valor Final ' + prodString)
+        #else:
+            #print('Altura Maxima encontrada. ' + prodString)
         return
     else:
         res = [ele for ele in nonTerminalSymbols if(ele in prodString)]
-        print(listProductions.get(res[0]))
         for prod in listProductions.get(res[0]):
             newString = prodString.replace(res[0], prod, 1)
-            print(newString)
+            print(printLines(counter) + newString)
             getNodesRec(enterString, newString, maxSizeTree, nonTerminalSymbols, listProductions, counter)
 
+
+def printLines(counter):
+    linesStr = '--'
+    while(counter != 1):
+        linesStr += '--'
+        counter-=1
+    return linesStr
 
     #if counter == 0:
     #    res = "S"
