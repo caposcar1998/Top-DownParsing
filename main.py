9ed2d67b478b0ee3511d4f35b543c061e110b11d
caposcar1998 <oscarcontrerascap@gmail.com>
2020-06-03 18:06:58 -0500

    se comenta todo menos topDown

--- main.py
+++ main.py
@@ -1,27 +1,48 @@
+#Tecnológico de Montettey
+#Computational mathematics
+#Proffesor Gilberto Huesca
+#Students:
+    #Oscar Contreras Palacios A01655772
+    #Mauricio Acosta
+#Tod down parsing tree 
+#Part2 project
+
+#If the string is found, the word "final" will appear after the string in the tree created
+
 from os import listdir
 from os.path import isfile, join
+#Libraries used to read txt files
 from obtainInfoFile import openFile
+#Import to get information segmented from top down aprsing file
 from topDownParsing import createParsingTree
+#Import with algorithm of top down parsing
+
 def main ():
     nonTerminalSymbols = None
     terminalSymbols = None
     startingSymbol = None
     listProductions = None
+    #Variables that will take value of the tuple that file selected will return
+
     print("Enter the string to check")
     enterString = input()
     print("Enter max depth of the tree")
     maxSizeTree = input()
+    #Inputs that the user will provide
     print("Select file from files")
     onlyfiles = [f for f in listdir("files") if isfile(join("files", f))]
     print(onlyfiles)
     fileSelected = input()
+    #Select file from files inside files folder
 
     nonTerminalSymbols = openFile(fileSelected)[0]
     terminalSymbols = openFile(fileSelected)[1]
     startingSymbol = openFile(fileSelected)[2]
     listProductions =openFile(fileSelected)[3]
+    #Assigning values from the tuple recieved from fileSelected
 
     createParsingTree(enterString, maxSizeTree, nonTerminalSymbols, startingSymbol, listProductions)
+    #Function from topDownParsing that will execute the algorithm
 
 if __name__ == "__main__":
     main()
--- obtainInfoFile.py
+++ obtainInfoFile.py
@@ -8,34 +8,41 @@ def openFile(fileName):
     dictionaryProds =  {
         "S": [],
         }
+    #Create variables in the start of the function so the could be used globally
     with open('files/' + fileName) as fp:
         line = fp.readline()
         cnt = 0
         while line:
+            #Check line by line
             if(cnt == 0):
                 nonTerminalSymbols = createTerminalSymbols(line)
                 createDictionariesToFill(nonTerminalSymbols,dictionaryProds)
+                #Check for nonTerminalSymbols and create a dictionary for every symbol
             if(cnt == 1):
                 terminalSymbols = createNoTerminalSymbols(line)
+                #Check for TerminalSymbols
             if(cnt == 2):
                 startingSymbol = line
+                #Check for startingSymbol
             if(cnt >= 3):
                 createListProduction(nonTerminalSymbols, line, dictionaryProds)
+                #From line 3 there are only pouctions, so we need to sort them by corresponding symbol
             line = fp.readline()
             cnt += 1
     fp.close()
 
     return nonTerminalSymbols, terminalSymbols, startingSymbol, dictionaryProds
+    #Values returned to main function
 
 def createDictionariesToFill(nonTerminalSymbols,dictionaryProds):
     for letter in nonTerminalSymbols:
         dictionaryProds[letter] = []
-
+    #Create dictionary for every letter in nonTerminalSymbols
 
 
 def createListProduction(nonTerminalSymbols,line, dictionaryProds):
     dictionaryProds[line[0]].append(line.split(">",1)[1].strip())
-    
+    #Here we get the information after the > sign, this information are the states
         
 
 def createTerminalSymbols(line):
@@ -45,6 +52,7 @@ def createTerminalSymbols(line):
     for element in nonTerminal:
         newList.append(element.strip())
     return newList
+    #A list with the terminal symbols is created
 
 def createNoTerminalSymbols(line):
     terminal = None
@@ -53,3 +61,4 @@ def createNoTerminalSymbols(line):
     for element in nonTerminal:
         newList.append(element.strip())
     return newList
+    #A list with the non terminal symbols is created
