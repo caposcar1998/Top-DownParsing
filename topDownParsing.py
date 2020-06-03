a5011c679d6ba7357bcff7c7c064d751cf2a17ee
caposcar1998 <oscarcontrerascap@gmail.com>
2020-06-03 17:35:17 -0500

    Se borra comentado y se anade excepcion

--- topDownParsing.py
+++ topDownParsing.py
@@ -15,20 +15,19 @@ def createParsingTree(enterString, maxSizeTree, nonTerminalSymbols,startingSymbo
 def getNodesRec(enterString, prodString, maxSizeTree, nonTerminalSymbols, listProductions, counter):
     counter += 1
     if(enterString == prodString or counter >= int(maxSizeTree)):
-        #if(enterString == prodString):
-            #print('Valor Final ' + prodString)
-        #else:
-            #print('Altura Maxima encontrada. ' + prodString)
         return
     else:
         res = [ele for ele in nonTerminalSymbols if(ele in prodString)]
         for prod in listProductions.get(res[0]):
-            newString = prodString.replace(res[0], prod, 1)
-            if (newString == enterString):
-                print(printLines(counter) + newString+ " final")
-            else:
-                print(printLines(counter) + newString)
-            getNodesRec(enterString, newString, maxSizeTree, nonTerminalSymbols, listProductions, counter)
+            try:
+                newString = prodString.replace(res[0], prod, 1)
+                if (newString == enterString):
+                    print(printLines(counter) + newString+ " final")
+                else:
+                    print(printLines(counter) + newString)
+                getNodesRec(enterString, newString, maxSizeTree, nonTerminalSymbols, listProductions, counter)
+            except:
+                print("End")
 
 
 def printLines(counter):
@@ -37,18 +36,3 @@ def printLines(counter):
         linesStr += '--'
         counter-=1
     return linesStr
-
-    #if counter == 0:
-    #    res = "S"
-
-
-    #if (bool(res) and counter < int(maxSizeTree) ):
-    #    for prod in  listProductions.get(res[0]):
-    #        stringChanged = enterString.replace(res[0],prod,1 )
-    #        #print(enterString.replace(res[0],prod,1))
-    #        counter +=1
-    #        print(prod)
-    #        print(stringChanged)
-    #        createParsingTree(stringChanged, maxSizeTree, nonTerminalSymbols,startingSymbol, listProductions, counter)
-    #else:
-    #    print("Your final string is: ",enterString)
